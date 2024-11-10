e0a76ac990be136fa48b275b6ffffac2a10cb1ef
David Niswonger <dniswonger@gmail.com>
2024-11-10 11:16:51 -0600

    trying to fix tailwind

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,7 +1,7 @@
-export default function PlantDatabase(){
-    return (
-        <div className="flex h-full items-center justify-center">
-            <p className="text-2xl text-white">Settings</p>
-        </div>
-    )
+export default function PlantDatabase() {
+  return (
+    <div className="flex h-full items-center justify-center bg-slate-300">
+      <p className="text-2xl text-white">Settings</p>
+    </div>
+  );
 }
