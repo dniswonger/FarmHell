701021521bd1506a1a1af2f1fcdbefdc8e0154cd
David Niswonger <dniswonger@liveearth.com>
2024-11-10 17:51:25 -0600

    add pages to cors list

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,16 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
+            },
+            {
+                // matching pages because we need to request blobs directly from the client
+                source: "/pages/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
             }
         ]
     }
